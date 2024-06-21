0699b9152151d2ad956392c19d3bd369dc31b094
Nikita Stroganov <findoss1@gmail.com>
2024-06-22 00:54:58 +0300

    fix types

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+setTimeout(() => {
+  window.Telegram.WebApp.expand();
+}, 2000);
